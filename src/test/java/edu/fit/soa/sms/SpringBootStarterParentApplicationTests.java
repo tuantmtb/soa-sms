35283bec4e450e73e36e1497265dac493f055d53
Trần Minh Tuấn <tuantmtb@gmail.com>
2017-03-02 09:24:16 +0700

    update view

--- src/test/java/edu/fit/soa/sms/SpringBootStarterParentApplicationTests.java
+++ src/test/java/edu/fit/soa/sms/SpringBootStarterParentApplicationTests.java
@@ -1,14 +1,6 @@
 package edu.fit.soa.sms;
 
 
-import org.junit.runner.RunWith;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SpringBootStarterParentApplicationTests {
 
 
